d7898100b7824ba259c2fec76f9eb414279472e6
dlqudtjs <dlqudtjs922@gmail.com>
2025-09-29 22:38:57 +0900

    fix(config): swagger 주소 cors 추가

--- web-api/src/main/kotlin/mong/poker/webapi/global/config/WebConfig.kt
+++ web-api/src/main/kotlin/mong/poker/webapi/global/config/WebConfig.kt
@@ -2,6 +2,7 @@ package mong.poker.webapi.global.config
 
 import mong.poker.webapi.global.auth.AuthArgumentResolver
 import mong.poker.webapi.global.auth.AuthInterceptor
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -13,6 +14,9 @@ class WebConfig(
     private val authArgumentResolver: AuthArgumentResolver,
     private val authInterceptor: AuthInterceptor,
 ) : WebMvcConfigurer {
+    @Value("\${swagger-server}")
+    private val swaggerServer: String? = null
+
     // 컨트롤러 메서드의 파라미터를 해석하는 커스텀 리졸버 추가
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(authArgumentResolver)
@@ -27,6 +31,7 @@ class WebConfig(
         registry.addMapping("/**")
             .allowedOrigins(
                 "http://localhost:3000",
+                swaggerServer
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
