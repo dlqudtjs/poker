288b0a0fa70ac2e10fca0e43a49a3d473ba81037
dlqudtjs <dlqudtjs922@gmail.com>
2025-09-30 22:09:33 +0900

    chore(error): error 메시지 한글로 수정

--- application/src/main/kotlin/mong/poker/application/global/support/exception/ErrorType.kt
+++ application/src/main/kotlin/mong/poker/application/global/support/exception/ErrorType.kt
@@ -6,16 +6,16 @@ enum class ErrorType(
     val message: String,
 ) {
     // user 관련
-    DUPLICATED_NICKNAME(400, "Duplicated nickname"),
-    DUPLICATED_ACCOUNT_ID(400, "Duplicated account id"),
-    INVALID_LOGIN_INFO(400, "Invalid login info"),
-    USER_NOT_FOUND(404, "User not found"),
+    DUPLICATED_NICKNAME(400, "닉네임이 이미 존재합니다."),
+    DUPLICATED_ACCOUNT_ID(400, "계정 ID가 이미 존재합니다."),
+    INVALID_LOGIN_INFO(400, "로그인 정보가 올바르지 않습니다."),
+    USER_NOT_FOUND(400, "유저를 찾을 수 없습니다."),
 
     // room 관련
-    ROOM_NOT_FOUND(400, "Room not found"),
+    ROOM_NOT_FOUND(400, "비밀번호가 일치하지 않습니다."),
 
-    UNCAUGHT_EXCEPTION(500, "Uncaught exception"),
-    BAD_REQUEST(400, "Bad request"),
-    NOT_FOUND(404, "Not found"),
-    UNAUTHORIZED(401, "Unauthorized"),
+    UNCAUGHT_EXCEPTION(500, "알 수 없는 오류입니다."),
+    BAD_REQUEST(400, "올바르지 않은 요청입니다."),
+    NOT_FOUND(404, "요청하신 리소스를 찾을 수 없습니다."),
+    UNAUTHORIZED(401, "인증이 필요합니다."),
 }
